13ed7dd087165335cbcfa29f0aac7006f79061c5
Pavel romanus <45469186+Shado2107@users.noreply.github.com>
2019-07-11 10:57:36 +0200

    push(11/07/19)
    slides sur la main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transformer_page_view/transformer_page_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -27,85 +29,134 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _slideIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+final GlobalKey<ScaffoldState> _key = new GlobalKey<ScaffoldState>();
 
+final List<String> images = [
+
+];
+
+List<Color> colors = [Colors.orange];
+
+final List<String> text0= [
+    "Welcome in your app",
+    "Enjoy teaching...",
+    "Showcase your skills",
+    "Friendship is great"
+];
+
+final List<String> text1 = [
+    "App for food lovers, satisfy your taste",
+    "Find best meals in your area, simply",
+    "Have fun while eating your relatives and more",
+    "Meet new friends from all over the world"
+  ];
+
+
+final IndexController controller = IndexController();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    TransformerPageView transformerPageView = TransformerPageView(
+        pageSnapping: true,
+        onPageChanged: (index){
+          setState(() {
+           this._slideIndex =index; 
+          });
+
+          switch(index){
+            case 2:
+               {
+                  print('Last slide: $index');
+               }
+          }
+        },
+        loop: false,
+        controller: controller,
+        transformer: new PageTransformerBuilder(
+          builder:(Widget child, TransformInfo info){
+        return new Material(
+          color: Colors.white,
+          elevation: 8.0,
+          textStyle: new TextStyle(
+           color: Colors.white),
+            borderRadius: new BorderRadius.circular(12.0),
+            child: new Container(
+              alignment: Alignment.center,
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new ParallaxContainer(
+                      child: new Text(
+                        text0[info.index],
+                        style: new TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 34.0,
+                          fontFamily: '',
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      position: info.position,
+                      opacityFactor: .8,
+                      translationFactor: 400.0,
+                    ),
+                    SizedBox(
+                      height: 45.0,
+                    ),
+                    new ParallaxContainer(
+                      child: new Image.asset(
+                        images[info.index],
+                        fit: BoxFit.contain,
+                        height: 350,
+                      ),
+                      position: info.position,
+                      translationFactor: 400.0,
+                    ),
+                    SizedBox(
+                      height: 45.0,
+                    ),
+                    new ParallaxContainer(
+                      child: new Text(
+                        text1[info.index],
+                        textAlign: TextAlign.center,
+                        style: new TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 28.0,
+                            fontFamily: 'Quicksand',
+                            fontWeight: FontWeight.bold),
+                      ),
+                      position: info.position,
+                      translationFactor: 300.0,
+                    ),
+                    SizedBox(
+                      height: 55.0,
+                    ),
+                    
+                  ],
+                )
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        );
+          }
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+   itemCount: 4  );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: transformerPageView,
     );
   }
 }
+
